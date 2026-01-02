a8305864345aa3572e5a06bb87c14a54f3167b49
M. Rizky Al Kusaeri <rizkyalkus12@gmail.com>
2026-01-02 07:46:15 +0700

    m

--- database/migrations/2025_12_30_232137_create_subjects_table.php
+++ database/migrations/2025_12_30_232137_create_subjects_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('subjects', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('status')->default('active');
             $table->timestamps();
         });
     }
